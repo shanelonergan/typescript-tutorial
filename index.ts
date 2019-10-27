21f38d00f00b520def9629434f667417e86ea080
shanelonergan <sptlonergan@gmail.com>
2019-10-27 18:31:57 -0400

    done

--- index.ts
+++ index.ts
@@ -1,12 +1,45 @@
-// Import stylesheets
-import './style.css';
-
-// Write TypeScript code!
-const appDiv: HTMLElement = document.getElementById('app');
-appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
-
 let a = 5;  
 let b = 5;  
 let c = a + b;
 
 console.log(c);
+
+let num: number = 5
+let name: string = 'shane'
+let isPresent: boolean = true
+
+// OO Typescript
+
+class Car {
+  model: String
+  doors: Number
+  isElectric: Boolean
+
+  constructor(model: String, doors: Number, isElectric: Boolean) {
+    this.model = model;  
+    this.doors = doors;  
+    this.isElectric = isElectric; 
+  }
+
+  displayMake(): void {
+    console.log(`This car is ${this.model}`)
+  }
+
+}
+
+const prius = new Car('Prius', 4, true)
+prius.displayMake()
+
+// interface
+
+interface ICar {
+  model: String
+  make: String 
+  display(): void
+} 
+
+const InterfaceCar: ICar = {
+  model: 'prius',
+  make: 'toyota',
+  display() { console.log('hi') }  
+}
