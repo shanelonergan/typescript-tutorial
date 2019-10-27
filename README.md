77252062e373e2890703511aa40e80f3f68ef70a
shanelonergan <sptlonergan@gmail.com>
2019-10-27 17:01:33 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-husnkf
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-husnkf)
---
+++ index.ts
@@ -0,0 +1,12 @@
+// Import stylesheets
+import './style.css';
+
+// Write TypeScript code!
+const appDiv: HTMLElement = document.getElementById('app');
+appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+
+let a = 5;  
+let b = 5;  
+let c = a + b;
+
+console.log(c);
